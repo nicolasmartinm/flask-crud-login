d808fcfddaa8a5c8ea3016e711b6b3ca8ad5f6d5
nicolasmartinm <ni123omartin@gmail.com>
2026-01-05 13:19:52 -0500

    Use DATABASE_URL from Railway

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import os
+import psycopg2
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_db():
+    return psycopg2.connect(DATABASE_URL)
+
 @app.route("/")
 def home():
-    return "API Flask funcionando en Railway 🚀"
+    return "API Flask + PostgreSQL OK 🔥"
